05b0d61efd1167a50f6d1e813967ee18cdd617d5
lenovo <Athif binu>
2023-01-27 00:28:32 +0530

    add router dom

--- frontent/src/components/Product.js
+++ frontent/src/components/Product.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
-const Product=()=> {
+const Product=({product})=> {
   return (
-    <Card
-  >
-       
+    <Card className='my-3 p-3 rounded'>
+       <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top'/>
+       </a>
+
+       <Card.Body>
+       <a href={`/product/${product._id}`}>
+         <Card.Title as='div'><strong>{product.name}</strong></Card.Title>
+       </a>
+
+       <Card.Text as='div'>
+          <Rating
+           value={product.rating}
+            text={`${product.numReviews} reviews`}
+        
+          />
+       </Card.Text>
+
+       <Card.Text as='h3'>
+         ${product.price}
+       </Card.Text>
+
+       </Card.Body>
     </Card>
+
+
   )
 }
 
--- frontent/src/products.js
+++ frontent/src/products.js
@@ -1,13 +1,14 @@
+
 const products = [
   {
     _id: '1',
     name: 'Airpods Wireless Bluetooth Headphones',
-    image: '../public/Images/airpods.jpg',
+    image: '../Images/airpods.jpg',
     description:
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     brand: 'Apple',
     category: 'Electronics',
-    price: 89.99,
+    price: 82.99,
     countInStock: 10,
     rating: 4.5,
     numReviews: 12,
@@ -15,7 +16,7 @@ const products = [
   {
     _id: '2',
     name: 'iPhone 11 Pro 256GB Memory',
-    image: '../public/Images/alexa.jpg',
+    image: '/Images/alexa.jpg',
     description:
       'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
     brand: 'Apple',
@@ -28,7 +29,7 @@ const products = [
   {
     _id: '3',
     name: 'Cannon EOS 80D DSLR Camera',
-    image: '../public/Images/camera.jpg',
+    image: '/Images/camera.jpg',
     description:
       'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
     brand: 'Cannon',
@@ -41,7 +42,7 @@ const products = [
   {
     _id: '4',
     name: 'Sony Playstation 4 Pro White Version',
-    image: '../public/Images/mouse.jpg',
+    image: '/Images/mouse.jpg',
     description:
       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
     brand: 'Sony',
@@ -54,7 +55,7 @@ const products = [
   {
     _id: '5',
     name: 'Logitech G-Series Gaming Mouse',
-    image: '../public/Images/phone.jpg',
+    image: '/Images/phone.jpg',
     description:
       'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
     brand: 'Logitech',
@@ -67,7 +68,7 @@ const products = [
   {
     _id: '6',
     name: 'Amazon Echo Dot 3rd Generation',
-    image: '../public/Images/playstation.jpg',
+    image: '/Images/playstation.jpg',
     description:
       'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
     brand: 'Amazon',
