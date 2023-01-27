236ee0c19fe1dd7d48e941ce4d6d2b35eae21a9a
lenovo <Athif binu>
2023-01-27 11:55:47 +0530

    dom version bug solve

--- frontent/src/App.js
+++ frontent/src/App.js
@@ -1,21 +1,33 @@
 import React from 'react'
+import { BrowserRouter as Router,Route } from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
 function App() {
   return (
-    <>
-      
-        <Header/>
+    
+  
+      <Router>
+              <Header/>
         <main className='py-4'>
-          <Container>
-          <HomeScreen/>
-          </Container>
-        </main>
-        <Footer/>
-    </>
+                  <Container>
+                     <Route path='/' component={HomeScreen} exact />
+                     <Route path='/product/:id' component={ProductScreen}  />
+                  </Container>
+              </main>
+              <Footer/>
+      </Router>
+  
+    
+    
   )
 }
 
 export default App
+
+
+
+//used to fist praiorty
