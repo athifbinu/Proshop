ccb21b8399573b6d3b7e36b73392d62217867c50
lenovo <Athif binu>
2023-01-27 17:40:59 +0530

    dom v bug solve

--- frontent/src/components/Header.js
+++ frontent/src/components/Header.js
@@ -1,25 +1,41 @@
 import React from 'react'
+import {LinkContainer} from 'react-router-bootstrap'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
 
 
+
+
 const Header = () => {
   return (
     <header>
-           <Navbar bg="dark" variant='dark' expand="sm" collapseOnSelect>
-      <Container >
-        <Navbar.Brand  href="/">Proshop</Navbar.Brand>
+        <Navbar bg="dark" variant='dark' expand="sm" collapseOnSelect>
+      <Container  >
+         <LinkContainer to='/'>
+              <Navbar.Brand >Proshop</Navbar.Brand>
+         </LinkContainer>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
 
            <Nav className='ml-auto'>
-                  <Nav.Link href="/cart">
+            
+                 <LinkContainer to='/cart' >
+                  <Nav.Link >
                     <i className='fas fa-shopping-cart'></i>Cart
-                    </Nav.Link>
-                  <Nav.Link href="/login"><i className='fas fa-user'></i> sign in</Nav.Link>
-           </Nav>
-         
+                   </Nav.Link>
+                   </LinkContainer>
+             
+                  <LinkContainer to='/login'>
+                  <Nav.Link >
+                   <i className='fas fa-user'></i> sign in
+                  </Nav.Link>  
+                  </LinkContainer>
+            
+        
+               </Nav>
+          
 
    
 
